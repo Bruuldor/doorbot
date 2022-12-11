62846cf54bf646b63ec21e4931d169b6772a2105
Bruuldor <51272963+Bruuldor@users.noreply.github.com>
2022-12-11 05:43:48 +0100

    Update doorbot.js

--- doorbot.js
+++ doorbot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const moment = require('moment');
 const client = new Discord.Client();
 
+const events = {};
+
 const scheduleEvent = (message, eventDate) => {
   // Parse the event date using moment.js
   const parsedEventDate = moment(eventDate, 'DD-MM-YYYY HH:mm');
@@ -20,10 +22,11 @@ const scheduleEvent = (message, eventDate) => {
   // The event object should have a title and a date field
   const event = {
     title: message.content,
-    date: parsedEventDate
+    date: parsedEventDate,
+    author: message.author.id,
+    signups: []
   };
-  // Save the event in the database
-  saveEvent(event);
+  const id = saveEvent(event);
 
   // Add a reaction collector to the message to allow users to sign up for the event
   const filter = (reaction, user) => {
@@ -31,6 +34,13 @@ const scheduleEvent = (message, eventDate) => {
   };
   message.awaitReactions(filter, { max: 1 }).then(collected => {
     const users = collected.users.array();
+    // Add the users who have signed up for the event to the event object
+    for (const user of users) {
+      event.signups.push(user);
+    }
+    // Update the event in the database
+    updateEvent(event);
+
     // Send a confirmation message to each user who signed up for the event
     for (const user of users) {
       user.send('You have signed up for the event. We will remind you 24 hours before the event starts.');
@@ -63,22 +73,20 @@ const remindUsers = (event) => {
 
 client.on('message', message => {
   // Check if the message starts with '!schedule'
-  if (message.content.startsWith('!schedule')) {
-    // Split the message into an array of words
-    const words = message.content.split(' ');
+if (message.content.startsWith('!schedule')) {
+// Split the message into an array of words
+const words = message.content.split(' ');
+  // The event date should be the second word in the message
+const eventDate = words[1];
 
-    // The event date should be the second word in the message
-    const eventDate = words[1];
-
-    // Schedule the event
-    scheduleEvent(message, eventDate);
+// Schedule the event
+scheduleEvent(message, eventDate);
 }
-
 // Check if the message is '!remove'
 if (message.content === '!remove') {
 // Get the event that the user has created from the database
 const event = getUserEvent(message.author.id);
-    // Check if the user has an event scheduled
+// Check if the user has an event scheduled
 if (!event) {
   return message.channel.send('You do not have any events scheduled.');
 }
@@ -92,12 +100,65 @@ message.channel.send('The event has been removed.');
 // This function is called every hour to remind the users of upcoming events
 setInterval(() => {
 // Get all the events from the database
-const events = getAllEvents();
+const allEvents = getAllEvents();
 
 // Iterate over each event and send reminders to the users
-for (const event of events) {
+for (const event of allEvents) {
 remindUsers(event);
 }
 }, 60 * 60 * 1000);
+
+const saveEvent = (event) => {
+// Generate a unique id for the event
+const id = generateId();
+
+// Save the event in the events object using the generated id as the key
+events[id] = event;
+
+return id;
+};
+
+const updateEvent = (event) => {
+// Update the event in the events object using its id
+events[event.id] = event;
+};
+
+const getEvent = (id) => {
+// Return the event with the specified id from the events object
+return events[id];
+};
+
+const getEventSignups = (event) => {
+// Return the list of users who have signed up for the event from the database
+return event.signups;
+};
+
+const getUserEvent = (userId) => {
+// Iterate over each event in the events object and return the first event that was created by the user
+for (const id in events) {
+const event = events[id];
+if (event.author === userId) {
+return event;
+}
+}
+
+// Return null if the user does not have any events scheduled
+return null;
+};
+
+const removeEvent = (event) => {
+// Remove the event from the events object using its id
+delete events[event.id];
+};
+
+const generateId = () => {
+// Generate a random id using the current timestamp and a random number
+return ${Date.now()}${Math.random()};
+};
+
+const getAllEvents = () => {
+// Return an array of all the events in the events object
+return Object.values(events);
+};
 
 client.login('your-bot-token-here');
